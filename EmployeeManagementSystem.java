c4e1372b3b108927fb46846e261370cc8241bc51
Nachobhai <121046332+Nachobhai@users.noreply.github.com>
2022-12-29 19:28:40 +0530

    Update EmployeeManagementSystem.java

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -5,9 +5,9 @@ class MainMenu
 {
   public void menu()
   {
-    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t******************************************");
     System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
-    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t******************************************");
     System.out.println("\n\nPress 1 : To Add an Employee Details");
     System.out.println("Press 2 : To See an Employee Details ");
     System.out.println("Press 3 : To Remove an Employee");
